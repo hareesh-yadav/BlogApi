9abbe4b286e40df31928976e4fb300ed05024eb7
harry <70253607+hareesh-yadav@users.noreply.github.com>
2022-06-06 11:53:09 +0530

    Update dbConfig.js

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -2,7 +2,7 @@ module.exports = {
     HOST: 'us-cdbr-east-05.cleardb.net',
     USER: 'b7a10c38526c04',
     PASSWORD: '5f8cf741',
-    DB: 'heroku_0b5bfcc46491252',
+    database: 'heroku_0b5bfcc46491252',
     dialect: 'mysql',
 
     pool: {
