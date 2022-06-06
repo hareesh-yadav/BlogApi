8841cfaccf87752ef9df369afaa16ef7ae975a26
harry <70253607+hareesh-yadav@users.noreply.github.com>
2022-06-06 17:56:07 +0530

    Update dbConfig.js

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -3,6 +3,7 @@ module.exports = {
     USER: 'b7a10c38526c04',
     PASSWORD: '5f8cf741',
     database: 'heroku_0b5bfcc46491252',
+    dialect: 'mysql',
     pool: {
         max: 5,
         min: 0,
